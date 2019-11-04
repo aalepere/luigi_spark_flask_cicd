82c48be6a668cc946b23cd858cc02b38fe534f7a
Arnaud <arnaud@hokodo.co>
2019-11-04 19:56:34 +0100

    unittest setUp

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,1 +1,12 @@
-import luigi
+from unittest import TestCase
+
+
+class PipelineModellingTest(TestCase):
+    """
+    """
+
+    def setUP(self):
+        pass
+
+    def test_1(self):
+        pass
